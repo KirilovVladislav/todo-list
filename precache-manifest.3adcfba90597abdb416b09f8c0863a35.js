385f453d37e5e5e22b4f639d0bae4c03ac3117fc
Kirilov Vladislav <kirilov.vl.ol@gmail.com>
2020-11-22 15:09:37 +0300

    Updates

---
+++ precache-manifest.3adcfba90597abdb416b09f8c0863a35.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f01fd08360380963de9da752e9d5095c",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "7390c294fbdb55a2f16d",
+    "url": "/todo-list/static/css/main.e46780ad.chunk.css"
+  },
+  {
+    "revision": "881201f2a168cfe29b3c",
+    "url": "/todo-list/static/js/2.83506094.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/todo-list/static/js/2.83506094.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7390c294fbdb55a2f16d",
+    "url": "/todo-list/static/js/main.873c5bd9.chunk.js"
+  },
+  {
+    "revision": "89a6e08d7efb69d221cb",
+    "url": "/todo-list/static/js/runtime-main.4b4c94fd.js"
+  },
+  {
+    "revision": "7869e84bdda526c2724a8d16fb6af33d",
+    "url": "/todo-list/static/media/preloader.7869e84b.svg"
+  }
+]);
